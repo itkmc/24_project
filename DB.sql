7fc592a3672914d042675d0eac1be0f070798717
itkmc <it0219@gmail.com>
2024-05-17 17:45:56 +0900

    DB 수정

--- DB.sql
+++ DB.sql
@@ -2,6 +2,16 @@ DROP DATABASE IF EXISTS `History_Timeline`;
 CREATE DATABASE `History_Timeline`;
 USE `History_Timeline`;
 
+# article 테이블 생성
+CREATE TABLE article(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+
 # 회원 테이블 생성
 CREATE TABLE MEMBER (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -70,137 +80,11 @@ CREATE TABLE csv2 (
     museumName CHAR(100) NOT NULL,
     latitude CHAR(100) NOT NULL,
     longitude CHAR(100) NOT NULL,
-    viewingHours CHAR(200) NOT NULL,
-    closedDays CHAR(200) NOT NULL,
-    admissionFee CHAR(200) NOT NULL,
-    exhibitionInformation CHAR(200) NOT NULL
+    viewingHours TEXT NOT NULL,
+    closedDays LONG NOT NULL,
+    admissionFee LONG NOT NULL,
+    exhibitionInformation TEXT NOT NULL
 );
-
-
-
-ALTER TABLE MEMBER
-ADD COLUMN 문제점수 INT UNSIGNED NOT NULL DEFAULT 0;
-
-UPDATE MEMBER
-SET grade = CASE
-    WHEN 문제점수 <= 3 THEN '초보자'
-    WHEN 문제점수 <= 6 THEN '중급자'
-    WHEN 문제점수 <= 10 THEN '고급자'
-    ELSE '관리자'
-END;
-
-###############################################
-   
-DROP TABLE IF EXISTS csv1;
-DROP TABLE IF EXISTS csv2; 
-
-SELECT * FROM question;
-
-SELECT * FROM `member`;
-
-SELECT * FROM csv1;
-SELECT * FROM csv2;
-    
-    
-###############################################
-
-# 서브쿼리
-SELECT A.*,
-IFNULL(SUM(RP.point),0) AS extra__sumReactionPoint,
-IFNULL(SUM(IF(RP.point > 0, RP.point, 0)),0) AS extra__goodReactionPoint,
-IFNULL(SUM(IF(RP.point < 0, RP.point, 0)),0) AS extra__badReactionPoint
-FROM (
-    SELECT A.*, M.nickname AS extra__writer 
-    FROM article AS A
-    INNER JOIN `member` AS M
-    ON A.memberId = M.id
-    ) AS A
-LEFT JOIN reactionPoint AS RP
-ON A.id = RP.relId AND RP.relTypeCode = 'article'
-GROUP BY A.id
-ORDER BY A.id DESC;
-
-
-
-
-
-
-SELECT ruinsname ,latitude, hardness
-FROM csv
-
-# reactionPoint 테이블 생성
-CREATE TABLE reactionPoint(
-    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    regDate DATETIME NOT NULL,
-    updateDate DATETIME NOT NULL,
-    memberId INT(10) UNSIGNED NOT NULL,
-    relTypeCode CHAR(50) NOT NULL COMMENT '관련 데이터 타입 코드',
-    relId INT(10) NOT NULL COMMENT '관련 데이터 번호',
-    `point` INT(10) NOT NULL
-);
-
-# reactionPoint 테스트 데이터 생성
-# 1번 회원이 1번 글에 싫어요
-INSERT INTO reactionPoint
-SET regDate = NOW(),
-updateDate = NOW(),
-memberId = 1,
-relTypeCode = 'article',
-relId = 1,
-`point` = -1;
-
-# 1번 회원이 2번 글에 좋아요
-INSERT INTO reactionPoint
-SET regDate = NOW(),
-updateDate = NOW(),
-memberId = 1,
-relTypeCode = 'article',
-relId = 2,
-`point` = 1;
-
-# 2번 회원이 1번 글에 싫어요
-INSERT INTO reactionPoint
-SET regDate = NOW(),
-updateDate = NOW(),
-memberId = 2,
-relTypeCode = 'article',
-relId = 1,
-`point` = -1;
-
-# 2번 회원이 2번 글에 싫어요
-INSERT INTO reactionPoint
-SET regDate = NOW(),
-updateDate = NOW(),
-memberId = 2,
-relTypeCode = 'article',
-relId = 2,
-`point` = -1;
-
-# 3번 회원이 1번 글에 좋아요
-INSERT INTO reactionPoint
-SET regDate = NOW(),
-updateDate = NOW(),
-memberId = 3,
-relTypeCode = 'article',
-relId = 1,
-`point` = 1;
-
-# article 테이블에 좋아요 관련 컬럼 추가
-ALTER TABLE article ADD COLUMN goodReactionPoint INT(10) UNSIGNED NOT NULL DEFAULT 0;
-ALTER TABLE article ADD COLUMN badReactionPoint INT(10) UNSIGNED NOT NULL DEFAULT 0;
-
-# update join -> 기존 게시물의 good,bad RP 값을 RP 테이블에서 가져온 데이터로 채운다
-UPDATE article AS A
-INNER JOIN (
-    SELECT RP.relTypeCode,RP.relId,
-    SUM(IF(RP.point > 0, RP.point, 0)) AS goodReactionPoint,
-    SUM(IF(RP.point < 0, RP.point * -1, 0)) AS badReactionPoint
-    FROM reactionPoint AS RP
-    GROUP BY RP.relTypeCode, RP.relId
-) AS RP_SUM
-ON A.id = RP_SUM.relId
-SET A.goodReactionPoint = RP_SUM.goodReactionPoint,
-A.badReactionPoint = RP_SUM.badReactionPoint;
 
 # reply 테이블 생성
 CREATE TABLE reply (
@@ -212,12 +96,47 @@ CREATE TABLE reply (
     `body`TEXT NOT NULL
 );
 
+# article TD 생성
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목1',
+`body` = '내용1';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목2',
+`body` = '내용2';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목3',
+`body` = '내용3';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목4',
+`body` = '내용4';
+
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
+
+UPDATE article
+SET memberId = 1
+WHERE id IN (1,2);
+
+
+UPDATE article
+SET memberId = 3
+WHERE id IN (3,4);
+
 # 2번 회원이 1번 글에 댓글 작성
 INSERT INTO reply
 SET regDate = NOW(),
 updateDate = NOW(),
 memberId = 2,
-relTypeCode = 'article',
 relId = 1,
 `body` = '댓글 1';
 
@@ -226,7 +145,6 @@ INSERT INTO reply
 SET regDate = NOW(),
 updateDate = NOW(),
 memberId = 2,
-relTypeCode = 'article',
 relId = 1,
 `body` = '댓글 2';
 
@@ -235,7 +153,6 @@ INSERT INTO reply
 SET regDate = NOW(),
 updateDate = NOW(),
 memberId = 3,
-relTypeCode = 'article',
 relId = 1,
 `body` = '댓글 3';
 
@@ -244,219 +161,32 @@ INSERT INTO reply
 SET regDate = NOW(),
 updateDate = NOW(),
 memberId = 2,
-relTypeCode = 'article',
 relId = 2,
 `body` = '댓글 4';
 
-# reply 테이블에 좋아요 관련 컬럼 추가
-ALTER TABLE reply ADD COLUMN goodReactionPoint INT(10) UNSIGNED NOT NULL DEFAULT 0;
-ALTER TABLE reply ADD COLUMN badReactionPoint INT(10) UNSIGNED NOT NULL DEFAULT 0;
+ALTER TABLE MEMBER
+ADD COLUMN 문제점수 INT UNSIGNED NOT NULL DEFAULT 0;
 
-# reactionPoint 테스트 데이터 생성
-# 1번 회원이 1번 댓글에 싫어요
-INSERT INTO reactionPoint
-SET regDate = NOW(),
-updateDate = NOW(),
-memberId = 1,
-relTypeCode = 'reply',
-relId = 1,
-`point` = -1;
+UPDATE MEMBER
+SET grade = CASE
+    WHEN 문제점수 <= 3 THEN '초보자'
+    WHEN 문제점수 <= 6 THEN '중급자'
+    WHEN 문제점수 <= 10 THEN '고급자'
+    ELSE '관리자'
+END;   
 
-# 1번 회원이 2번 댓글에 좋아요
-INSERT INTO reactionPoint
-SET regDate = NOW(),
-updateDate = NOW(),
-memberId = 1,
-relTypeCode = 'reply',
-relId = 2,
-`point` = 1;
+##########################################################
 
-# 2번 회원이 1번 댓글에 싫어요
-INSERT INTO reactionPoint
-SET regDate = NOW(),
-updateDate = NOW(),
-memberId = 2,
-relTypeCode = 'reply',
-relId = 1,
-`point` = -1;
+SELECT * FROM question;
 
-# 2번 회원이 2번 댓글에 싫어요
-INSERT INTO reactionPoint
-SET regDate = NOW(),
-updateDate = NOW(),
-memberId = 2,
-relTypeCode = 'reply',
-relId = 2,
-`point` = -1;
+SELECT * FROM `member`;
 
-# 3번 회원이 1번 댓글에 좋아요
-INSERT INTO reactionPoint
-SET regDate = NOW(),
-updateDate = NOW(),
-memberId = 3,
-relTypeCode = 'reply',
-relId = 1,
-`point` = 1;
+SELECT * FROM csv1;
 
-# update join -> 기존 게시물의 good,bad RP 값을 RP 테이블에서 가져온 데이터로 채운다
-UPDATE reply AS R
-INNER JOIN (
-    SELECT RP.relTypeCode,RP.relId,
-    SUM(IF(RP.point > 0, RP.point, 0)) AS goodReactionPoint,
-    SUM(IF(RP.point < 0, RP.point * -1, 0)) AS badReactionPoint
-    FROM reactionPoint AS RP
-    GROUP BY RP.relTypeCode, RP.relId
-) AS RP_SUM
-ON R.id = RP_SUM.relId
-SET R.goodReactionPoint = RP_SUM.goodReactionPoint,
-R.badReactionPoint = RP_SUM.badReactionPoint;
+SELECT * FROM csv2;
 
-
-
-
-SELECT * FROM article;
-
-
-
-SELECT * FROM `board`;
-
-SELECT * FROM reactionPoint;
-
-SELECT * FROM `reply`;
-
-DELETE FROM reply WHERE id = 3
-
-SELECT goodReactionPoint
-FROM article 
-WHERE id = 1
-
-INSERT INTO article
-(
-    regDate, updateDate, memberId, boardId, title, `body`
-)
-SELECT NOW(),NOW(), FLOOR(RAND() * 2) + 2, FLOOR(RAND() * 3) + 1, CONCAT('제목_',RAND()), CONCAT('내용_',RAND())
-FROM article;
-
-SELECT IFNULL(SUM(RP.point),0)
-FROM reactionPoint AS RP
-WHERE RP.relTypeCode = 'article'
-AND RP.relId = 3
-AND RP.memberId = 1;
-
-
-UPDATE article 
-SET title = '제목5'
-WHERE id = 5;
-
-UPDATE article 
-SET title = '제목11'
-WHERE id = 6;
-
-UPDATE article 
-SET title = '제목45'
-WHERE id = 7;
-
-SELECT FLOOR(RAND() * 2) + 2
-
-SELECT FLOOR(RAND() * 3) + 1
-
-
-SHOW FULL COLUMNS FROM `member`;
-DESC `member`;
-
-
-
-SELECT LAST_INSERT_ID();
-
-SELECT *
-FROM article AS A
-WHERE 1
-
-	AND boardId = 1
-
-			AND A.title LIKE CONCAT('%','0000','%')
-			OR A.body LIKE CONCAT('%','0000','%')
-
-ORDER BY id DESC
-
-SELECT COUNT(*)
-FROM article AS A
-WHERE 1
-AND boardId = 1
-AND A.title LIKE CONCAT('%','0000','%')
-OR A.body LIKE CONCAT('%','0000','%')
-ORDER BY id DESC
-
-
-SELECT hitCount
-FROM article
-WHERE id = 374;
-
-SELECT A.*
-FROM article AS A
-WHERE A.id = 1
-
-SELECT A.*, M.nickname AS extra__writer
-FROM article AS A
-INNER JOIN `member` AS M
-ON A.memberId = M.id
-WHERE A.id = 1
-
-# LEFT JOIN
-SELECT A.*, M.nickname AS extra__writer, RP.point
-FROM article AS A
-INNER JOIN `member` AS M
-ON A.memberId = M.id
-LEFT JOIN reactionPoint AS RP
-ON A.id = RP.relId AND RP.relTypeCode = 'article'
-GROUP BY A.id
-ORDER BY A.id DESC;
-
-# 서브쿼리
-SELECT A.*,
-IFNULL(SUM(RP.point),0) AS extra__sumReactionPoint,
-IFNULL(SUM(IF(RP.point > 0, RP.point, 0)),0) AS extra__goodReactionPoint,
-IFNULL(SUM(IF(RP.point < 0, RP.point, 0)),0) AS extra__badReactionPoint
-FROM (
-    SELECT A.*, M.nickname AS extra__writer 
-    FROM article AS A
-    INNER JOIN `member` AS M
-    ON A.memberId = M.id
-    ) AS A
-LEFT JOIN reactionPoint AS RP
-ON A.id = RP.relId AND RP.relTypeCode = 'article'
-GROUP BY A.id
-ORDER BY A.id DESC;
-
-# 조인
-SELECT A.*, M.nickname AS extra__writer,
-IFNULL(SUM(RP.point),0) AS extra__sumReactionPoint,
-IFNULL(SUM(IF(RP.point > 0, RP.point, 0)),0) AS extra__goodReactionPoint,
-IFNULL(SUM(IF(RP.point < 0, RP.point, 0)),0) AS extra__badReactionPoint
-FROM article AS A
-INNER JOIN `member` AS M
-ON A.memberId = M.id
-LEFT JOIN reactionPoint AS RP
-ON A.id = RP.relId AND RP.relTypeCode = 'article'
-GROUP BY A.id
-ORDER BY A.id DESC;
-
-
-SELECT *, COUNT(*)
-FROM reactionPoint AS RP
-GROUP BY RP.relTypeCode,RP.relId
-
-SELECT IF(RP.point > 0, '큼','작음')
-FROM reactionPoint AS RP
-GROUP BY RP.relTypeCode,RP.relId
-
-# 각 게시물의 좋아요, 싫어요 갯수
-SELECT RP.relTypeCode, RP.relId,
-SUM(IF(RP.point > 0,RP.point,0)) AS goodReactionPoint,
-SUM(IF(RP.point < 0,RP.point * -1,0)) AS badReactionPoint
-FROM reactionPoint AS RP
-GROUP BY RP.relTypeCode,RP.relId
-
-			SELECT answer
-			FROM question
-			WHERE id =1
+SELECT * FROM reply;
+   
+DROP TABLE csv2;
+    
+###############################################
